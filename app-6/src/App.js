64b8ce3c0e492b6ddbe8f4856a3941ac0155da81
Bret Fernberg <bret.fernberg@gmail.com>
2021-04-21 16:36:31 -0600

    Finished app 6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDo from './ToDo'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      tasks: ['clean kitchen', 'wipe counters', 'scrub toilet', 'make dinner'],
+      userInput: ''
+    }
+
+    this.addToList = this.addToList.bind(this);
+  }
+
+  addToList(){
+    this.setState({ tasks: [...this.state.tasks, this.state.userInput], userInput: '' })
+  }
+
+  handleChanges(value){
+    this.setState({ userInput: value })
+  }
+
+  render(){
+    let todo = this.state.tasks.map((el, i)=>{
+      return <ToDo key={i} task={el} />
+  })
+    return (
+      <div className="App">
+        <h1>My To-Do List</h1>
+        <input value={this.state.userInput} placeholder={'Add new task'} onChange={e=>this.handleChanges(e.target.value)} />
+        <button onClick={this.addToList}>Add</button>
+        {todo}
+      </div>
+    );
+
+  }
 }
 
 export default App;
---
+++ app-6/src/ToDo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ToDo(props){
+    
+    return(
+        <div>
+            <h3>{props.task}</h3>
+        </div>
+    )
+}
+
+export default ToDo;
